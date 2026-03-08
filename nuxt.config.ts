e5a4ecf2c1bf22916a6ae9edcb087e2edf1320a9
KatharinaMat <katharina.matkevits@gmail.com>
2026-03-08 19:34:44 +0200

    add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,16 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+
+  app: {
+    head: {
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/images/primastella_favicon.ico",
+        },
+      ],
+    },
+  },
 });
